41938f87b377b8637e8662cab9a62d1191186cc1
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-06-06 11:36:34 +0200

    first commit

---
+++ exercise-7.js
@@ -0,0 +1,42 @@
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get name() {
+    return this.firstName;
+  }
+
+  set name(value) {
+    return (this.firstName = value);
+  }
+
+  get surname() {
+    return this.lastName;
+  }
+
+  set surname(value) {
+    this.lastName = value;
+  }
+
+  get ages() {
+    return this.age;
+  }
+
+  set ages(value) {
+    this.age = value;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person = new Person("Mario", "Rossi", 25);
+console.log(person.fullName);
+
+person.firstName = "Maria";
+person.lastName = "Verdi";
+console.log(person.fullName);
